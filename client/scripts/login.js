1a710d8a40e0ecc879ada7ab01e6702e4bdb2e92
Shayen <radltaylor189@gmail.com>
2024-10-10 15:34:30 +0000

    test

--- client/scripts/login.js
+++ client/scripts/login.js
@@ -1,10 +1,17 @@
 const token = window.localStorage.getItem("token");
 if (token !== null) {
-    
+    fetch(`/verify/${token}`, (response) => {
+        alert(response)
+    })
 }
 
 function register() {
-
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    console.log(`Registering ${username} ${password}`)
+    fetch(`register/${username}/${password}`, (response) => {
+        alert(response);
+    });
 }
 
 function login() {
